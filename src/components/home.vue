f7426601dac5e8af59b2a9ca86eaa8187d2aceab
Baronno <falconhui@icloud.com>
2021-01-13 21:24:13 +1100

    Head page update

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">Exit </el-button>
-  </div>
+  <el-container class="home-containerbox">
+    <el-header>
+      <div>
+        <img src="../assets/Logo(已去底).png" alt="">
+        <span>HOTEL MANAGEMENT SYSTEM</span>
+      </div>
+      <el-button type="info" @click="logout">Exit </el-button></el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,4 +25,31 @@ export default {
 </script>
 
 <style lang="less">
+.home-containerbox{
+  height: 100%;
+  }
+.el-header {
+  background-color: rgba(6, 10, 10, 0.133);
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color:#fff;
+  font-size: 20px;
+  div{
+    display: flex;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: rgba(6, 10, 10, 0.133);
+}
+
+.el-main {
+  background-color: rgba(19, 15, 15, 0.2)
+}
 </style>
