84b521e72e3f8fd9626358ac9fd78216a0bd272d
Baronno <falconhui@icloud.com>
2021-01-14 13:12:56 +1100

    axios authorization function update

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,12 +2,38 @@
   <el-container class="home-containerbox">
     <el-header>
       <div>
-        <img src="../assets/Logo(已去底).png" alt="">
+        <img src="../assets/Logo(已去底).png" alt="" />
         <span>HOTEL MANAGEMENT SYSTEM</span>
       </div>
-      <el-button type="info" @click="logout">Exit </el-button></el-header>
+      <el-button type="info" @click="logout">Exit </el-button></el-header
+    >
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- this is the aside menu -->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#303133"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+        <!-- the level 1 menu -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Navigator One</span>
+            </template>
+            <!-- Level 2 menu -->
+              <el-menu-item index="1-1">
+                <template slot="title">
+                  <!-- Icon -->
+              <i class="el-icon-location"></i>
+              <!-- word -->
+              <span>酒店用品</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -25,31 +51,31 @@ export default {
 </script>
 
 <style lang="less">
-.home-containerbox{
+.home-containerbox {
   height: 100%;
-  }
+}
 .el-header {
-  background-color: rgba(6, 10, 10, 0.133);
+  background-color: #303133;
   display: flex;
   justify-content: space-between;
   padding-left: 0;
   align-items: center;
-  color:#fff;
+  color: #fff;
   font-size: 20px;
-  div{
+  div {
     display: flex;
     align-items: center;
-    span{
+    span {
       margin-left: 15px;
     }
   }
 }
 
 .el-aside {
-  background-color: rgba(6, 10, 10, 0.133);
+  background-color: #303133;
 }
 
 .el-main {
-  background-color: rgba(19, 15, 15, 0.2)
+  background-color: rgba(19, 15, 15, 0.2);
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -8,5 +8,9 @@ Vue.use(Input)
 Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 Vue.use(Main)
 Vue.prototype.$message = Message
