bd4e81b116b4f57912d07229e4320681f7463b33
renhui zhou <40229660+Baronno@users.noreply.github.com>
2021-03-23 18:47:19 +1100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import router from './router'
 import './plugins/element.js'
 import axios from 'axios'
 /* deploy the root path of request */
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://ecommerce001.netlify.app/api/private/v1/login'
 /* get data from server */
 axios.interceptors.request.use(config => {
   console.log(config)
